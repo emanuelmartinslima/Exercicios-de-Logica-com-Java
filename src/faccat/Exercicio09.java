9deb6ac888e8461a624ceac065230ba3ecc4cde7
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-03 12:39:21 -0300

    Adição do exercício 09 da faccat

---
+++ src/faccat/Exercicio09.java
@@ -0,0 +1,25 @@
+package faccat;
+
+import java.util.Scanner;
+
+public class Exercicio09 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Cálculo de Salário Reajustado");
+		
+		System.out.println("Digite o valor do salário mensal atual do funcionário: ");
+		float salario = scanner.nextFloat();
+		
+		System.out.println("Digite o valor do percentual de reajuste do salário (em %): ");
+		float percentualReajuste = scanner.nextFloat();
+		
+		System.out.printf("Salário Antigo: R$ %.2f\nPercentual de Reajuste: %.2f %%", salario, percentualReajuste);
+		
+		salario = salario * (1 + (percentualReajuste / 100));
+		
+		System.out.printf("\nSalário Reajustado: %.2f", salario);
+		
+		scanner.close();
+	}
+}
