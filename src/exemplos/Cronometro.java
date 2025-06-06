e462561db3fe9bd0f169ea40e26543eb58a0c566
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-06 17:10:19 -0300

    Implementação de break com laço de repetição for

---
+++ src/exemplos/Cronometro.java
@@ -0,0 +1,13 @@
+package exemplos;
+
+public class Cronometro {
+	public static void main(String[] args) {
+		for(int horas = 24; horas >= 0; horas--) {
+			for(int minutos = 59; minutos >= 0; minutos--) {
+				for(int segundos = 59; segundos >= 0; segundos--) {
+					System.out.printf("%d : %d : %d%n", horas, minutos, segundos);
+				}
+			}
+		}
+	}
+}
