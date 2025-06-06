2ae5e2a3cd6c716e3665635a79e0f1117ffb593a
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-06 17:43:39 -0300

    Implementação de array com laço de repetição for

---
+++ src/exemplos/Array03.java
@@ -0,0 +1,17 @@
+package exemplos;
+
+public class Array03 {
+	public static void main(String[] args) {
+		String[] nomes = {"Letícia", "Marcos", "Marcus", "Rafael", "Vinicius", "Vitor"};
+		
+		for(int indice = 0; indice < 5; indice++) {
+			System.out.println(nomes[indice]);
+		}
+		
+		System.out.println("");
+		
+		for(int indice = 0; indice < nomes.length - 1; indice++) {
+			System.out.println(nomes[indice]);
+		}
+	}
+}
