211ad7073ba3a1e4d8dd80bb9de40eb69452b9ba
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-09 15:47:14 -0300

    Implementação de exemplo 01 de matriz

--- src/exemplos/ArrayForEach03.java
+++ src/exemplos/ArrayForEach03.java
@@ -2,14 +2,12 @@ package exemplos;
 
 public class ArrayForEach03 {
 	public static void main(String[] args) {
-		int[] idades = {40, 89, 35, 50, 27, 42, 5, 10};
+		int[] idades = {40, 89, 1, 50, 27, 42, 5, 10};
 		
 		int menorIdade = idades[0];
 		
 		for(int idade : idades) {
-			if(menorIdade > idade) {
-				menorIdade = idade;
-			}
+			menorIdade = (idade < menorIdade) ? idade : menorIdade;
 		}
 		
 		System.out.printf("Menor Idade: %d", menorIdade);
