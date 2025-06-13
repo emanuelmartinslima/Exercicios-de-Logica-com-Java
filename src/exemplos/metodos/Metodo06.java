dd4900328147bd19c17c2212608a56720b94cbd2
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-13 15:43:11 -0300

    Implementação de exemplo 06 de métodos

---
+++ src/exemplos/metodos/Metodo06.java
@@ -0,0 +1,11 @@
+package exemplos.metodos;
+
+public class Metodo06 {
+	static int calcularSoma(int numero1, int numero2) {
+		return numero1 + numero2;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(calcularSoma(1, 2));
+	}
+}
