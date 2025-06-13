27665026c5f711f2409be9acb56e4b74e0228051
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-13 15:42:33 -0300

    Implementação de exemplo 05 de métodos

---
+++ src/exemplos/metodos/Metodo05.java
@@ -0,0 +1,12 @@
+package exemplos.metodos;
+
+public class Metodo05 {
+	static int calcularSoma(int numero) {
+		return 5 + numero;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(calcularSoma(10));
+		System.out.println(calcularSoma(-5));
+	}
+}
