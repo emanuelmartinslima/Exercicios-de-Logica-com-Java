ba51d05be09b96b18b04b9e42dda7f884ee0d990
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-16 15:47:40 -0300

    Implementação de novo código no exemplo 01 de construtores

--- src/exemplos/construtores/Construtor01.java
+++ src/exemplos/construtores/Construtor01.java
@@ -11,7 +11,11 @@ public class Construtor01 {
 	
 	public static void main(String[] args) {
 		Construtor01 carro = new Construtor01(1969, "Mustang");
+		Construtor01 carro02 = new Construtor01(2000, "Kwid");
+		Construtor01 carro03 = new Construtor01(2010, "Ferrari");
 		
 		System.out.printf("Nome do Modelo do Carro: %s%nAno do Carro: %d", carro.nomeModelo, carro.anoModelo);
+		System.out.printf("%n%nNome do Modelo do Carro: %s%nAno do Carro: %d", carro02.nomeModelo, carro02.anoModelo);
+		System.out.printf("%n%nNome do Modelo do Carro: %s%nAno do Carro: %d", carro03.nomeModelo, carro03.anoModelo);
 	}
 }
