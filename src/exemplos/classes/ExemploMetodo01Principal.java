a374bfcd6f584ed2dcff622ef1c225947f27ee40
Emanuel Martins de Lima <emanuelmartins1081@gmail.com>
2025-06-16 15:14:16 -0300

    Implementação de exemplo 01 de métodos da classe

---
+++ src/exemplos/classes/ExemploMetodo01Principal.java
@@ -0,0 +1,10 @@
+package exemplos.classes;
+
+public class ExemploMetodo01Principal {
+	public static void main(String[] args) {
+		ExemploMetodo01Secundario carro = new ExemploMetodo01Secundario();
+		
+		carro.aceleracaoTotal();
+		carro.velocidadeMaxima(200);
+	}
+}
---
+++ src/exemplos/classes/ExemploMetodo01Secundario.java
@@ -0,0 +1,11 @@
+package exemplos.classes;
+
+public class ExemploMetodo01Secundario {
+	public void aceleracaoTotal() {
+		System.out.println("Indo à velocidade máxima!!!");
+	}
+	
+	public void velocidadeMaxima(int velocidadeMaxima) {
+		System.out.println("Velocidade Máxima: " + velocidadeMaxima);
+	}
+}
